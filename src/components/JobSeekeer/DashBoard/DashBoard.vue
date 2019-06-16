b94f29081b17de08e39dd6cce0c38aa326dac5c3
nguyen <thaonguyennguyen1197@gmail.com>
2019-06-16 16:07:03 +0700

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 09bba351856723db52d5f8ddcc569db7a0a0556b, reversing
    changes made to e9760396585561534db8093dec7c289d933abd5c.

--- src/components/JobSeekeer/DashBoard/DashBoard.vue
+++ src/components/JobSeekeer/DashBoard/DashBoard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid fill-height>
     <v-layout align-center justify-center >
-      aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      abc1111111
     </v-layout>
   </v-container>
 </template>
