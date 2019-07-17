673d933bc98d01008cff965f4a4fcc8232189088
HAITTSE62631\USER <haitt19971212@gmail.com>
2019-07-17 18:31:12 +0900

    Update AdminViewAllCompany.vue

--- src/components/Admin/Company/AdminViewAllCompany.vue
+++ src/components/Admin/Company/AdminViewAllCompany.vue
@@ -18,13 +18,13 @@
 
 
           <v-flex xs2>
-            <v-btn style="height: 56px; position: relative; bottom: 5px" color="primary" @click="getAllJob()">Tìm</v-btn>
+            <v-btn style="height: 56px; position: relative; bottom: 5px" color="primary" @click="getAllCompany()">Tìm</v-btn>
           </v-flex>
         </v-flex>
 
 
         <v-data-table
-          :items="Job"
+          :items="Company"
           :headers="headers"
           :loading="loading"
           :pagination.sync="pagination"
@@ -41,8 +41,8 @@
           </template>
 
           <template #items="{item}">
-            <td  class="text-xs">{{item.title}}</td>
-            <td  class="text-xs">{{item.companyByCompanyId.nameCompany}}</td>
+            <td  class="text-xs">{{item.nameCompany}}</td>
+            <td  class="text-xs">{{item.cityByCityId.fullName}}</td>
             <td  class="text-xs">{{moment(item.createdDate).format('DD-MM-YYYY')}}</td>
             <td  class="text-xs-left">
               <v-chip color="error" v-if="item.status === 'new'">
@@ -51,8 +51,8 @@
               <v-chip color="success" v-if="item.status === 'approved'">
                 <span style="color: white !important;">Đã duyệt</span>
               </v-chip>
-              <v-chip color="grey" v-if="item.status === 'spam'">
-                <span style="color: white !important;">Spam</span>
+              <v-chip color="grey" v-if="item.status === 'deny'">
+                <span style="color: white !important;">Từ chối duyệt</span>
               </v-chip>
 
             </td>
@@ -88,199 +88,21 @@
       v-model="dialog"
       max-width="1200px">
       <v-card>
-        <v-card-title class="headline"><b>Thông tin công việc</b></v-card-title>
+        <v-card-title class="headline"><b>Thông tin công ty</b></v-card-title>
 
         <v-divider></v-divider>
         <v-card-text>
           <v-container class=" mb-3" fluid grid-list-md style=" border: 1px solid red; border-radius: 5px;">
             <v-layout row wrap>
               <v-flex d-flex xs12 sm6 md3 class="align-center pa-0">
-                <v-img src="https://www.seekpng.com/png/detail/25-257121_icon-big-image-png-camera-icon.png"  contain aspect-ratio="2"  v-if="!jobFull.company.logoImg"/>
-                <v-img contain :src="jobFull.company.logoImg" aspect-ratio="2" v-if="jobFull.company.logoImg"></v-img>
+                <v-img src="https://www.seekpng.com/png/detail/25-257121_icon-big-image-png-camera-icon.png"  contain aspect-ratio="2"  v-if="!companyDetail.logoImg"/>
+                <v-img contain :src="companyDetail.logoImg" aspect-ratio="2" v-if="companyDetail.logoImg"></v-img>
               </v-flex>
-              <v-flex d-flex xs12 sm6 md6>
-
-                <v-layout row wrap class="pa-0 ma-0" >
-
-                  <v-flex d-flex class="pa-0 ma-0" md12>
-                    <h1>{{jobFull.title}}</h1>
-                  </v-flex>
-                  <v-flex d-flex class="pa-0 ma-0" md12>
-                    <v-layout row wrap class="pa-0 ma-0">
-                      <v-flex d-flex xs12>
-                        <span> <b>Công ty:</b> {{jobFull.company.nameCompany}}</span>
-                      </v-flex>
-                      <v-flex d-flex xs12 v-if="jobFull.salaryTo === 0 && jobFull.salaryFrom > 0">
-                        <span> <b>Mức lương: </b> từ {{jobFull.salaryFrom}}đ trở lên</span>
-                      </v-flex>
-                      <v-flex d-flex xs12 v-if="jobFull.salaryFrom === 0 && jobFull.salaryTo > 0">
-                        <span> <b>Mức lương:</b> lên đến {{jobFull.salaryTo}}đ</span>
-                      </v-flex>
-                      <v-flex d-flex xs12 v-if="jobFull.salaryTo > 0 && jobFull.salaryFrom > 0">
-                        <span> <b>Mức lương:</b> từ {{jobFull.salaryFrom}}đ đến {{jobFull.salaryTo}}đ</span>
-                      </v-flex>
-                      <v-flex d-flex xs12 v-if="jobFull.salaryTo === 0 && jobFull.salaryFrom === 0">
-                        <span><b>Mức lương:</b> thương lượng</span>
-                      </v-flex>
-                      <v-flex d-flex xs12>
-                        <!--                      <span>View will stay here</span> -->
-                        <span><b>Khu vực:</b> {{jobFull.city.fullName}} <v-divider vertical class="ml-2 mr-2"></v-divider>
-                        <b>Ngày hết hạn nộp:</b> {{jobFull.endDateForApply}}</span>
-                      </v-flex>
-                      <v-flex d-flex xs12 fill-height>
-
-                      </v-flex>
-                    </v-layout>
-
-                  </v-flex>
-                </v-layout>
-
-              </v-flex>
+              abc
             </v-layout>
           </v-container>
           <!--        tab-->
-          <template  >
-            <v-tabs
-              color="cyan"
-              dark
-              next-icon="mdi-arrow-right-bold-box-outline"
-              prev-icon="mdi-arrow-left-bold-box-outline"
-              show-arrows
-            >
-              <v-tabs-slider color="yellow"></v-tabs-slider>
-              <v-tab
-                v-for="item in items"
-                :key="item"
-              >
-                {{ item }}
-              </v-tab>
 
-              <v-tabs-items >
-                <v-tab-item
-                  v-for="item in items"
-                  :key="item"
-                >
-                  <v-card flat>
-                    <!--card content-->
-
-                    <!--                  Thông Tin-->
-                    <v-card-text v-if="item === 'Thông Tin'">
-                      <v-container fluid fill-height>
-                        <v-layout align-center justify-center >
-                          <v-flex d-flex md12>
-
-                            <v-flex md8 sm12>
-                              <h4>Quyền lợi ứng viên</h4>
-                              <v-flex md12 v-html="jobFull.candidateBenefits" class="pb-3">
-                              </v-flex>
-                              <h4>Thông tin công việc</h4>
-                              <v-flex md12 v-html="jobFull.jobDescription" class="pb-3">
-                              </v-flex>
-                              <h4>Yêu cầu công việc</h4>
-                              <v-flex md12 v-html="jobFull.additionalRequest" class="pb-3">
-                              </v-flex>
-                            </v-flex>
-
-                            <v-flex md4 sm12>
-                              <v-card flat>
-                                <v-card-text >
-                                  <div style="border: 1px solid rgba(0,185,242,.5);" class="pa-3">
-
-                                    <v-text-field class="not-active"
-                                                  label="Ngày Đăng Tuyển"
-                                                  v-model="jobFull.createdDate"
-                                                  prepend-icon="mdi-calendar-clock"
-                                                  readonly
-                                                  color="none"
-                                    ></v-text-field>
-                                    <v-text-field class="not-active"
-                                                  label="Cấp Bậc"
-                                                  v-model="jobFull.joblevelName"
-                                                  prepend-icon="mdi-account "
-                                                  readonly
-                                                  color="none"
-                                    ></v-text-field>
-                                    <v-text-field class="not-active"
-                                                  label="Địa chỉ làm việc"
-                                                  v-model="jobFull.address"
-                                                  prepend-icon="mdi-map-marker"
-                                                  readonly
-                                                  color="none"
-                                    ></v-text-field>
-                                    <v-text-field class="not-active"
-                                                  label="Kĩ năng yêu cầu"
-                                                  v-model="jobFull.listSkillName"
-                                                  prepend-icon="mdi-account-star"
-                                                  readonly
-                                                  color="none"
-                                    ></v-text-field>
-                                    <!--                                  <v-text-field class="not-active"-->
-                                    <!--                                                label="Ngôn Ngữ Trình Bày"-->
-                                    <!--                                                value="Bất kỳ"-->
-                                    <!--                                                prepend-icon="mdi-calendar-clock"-->
-                                    <!--                                                readonly-->
-                                    <!--                                                color="none"-->
-                                    <!--                                  ></v-text-field>-->
-                                  </div>
-                                </v-card-text>
-                              </v-card>
-
-                            </v-flex>
-                          </v-flex>
-                        </v-layout>
-                      </v-container>
-                    </v-card-text>
-                    <!--                  End Thông Tin-->
-
-                    <!--                  Công Ty-->
-                    <v-card-text v-if="item === 'Công Ty'">
-                      <v-container fluid fill-height>
-                        <v-layout align-center justify-center >
-                          <v-flex d-flex md12>
-
-                            <v-flex md8 sm12>
-                              <h4>Thông tin công ty</h4>
-                              <v-flex md12 v-html="jobFull.company.description">
-
-                              </v-flex>
-                            </v-flex>
-
-                            <v-flex md4 sm12>
-                              <v-card flat>
-                                <v-card-text >
-                                  <div style="border: 1px solid rgba(0,185,242,.5);" class="pa-3">
-                                    <v-text-field class="not-active"
-                                                  label="Địa điểm"
-                                                  v-model="jobFull.company.address"
-                                                  prepend-icon="mdi-map-marker"
-                                                  readonly
-                                                  color="none"
-                                    ></v-text-field>
-                                    <v-text-field class="not-active"
-                                                  label="Số điện thoại liên hệ"
-                                                  v-model="jobFull.company.telephoneNumber"
-                                                  prepend-icon="mdi-phone"
-                                                  readonly
-                                                  color="none"
-                                    ></v-text-field>
-                                  </div>
-                                </v-card-text>
-                              </v-card>
-
-                            </v-flex>
-                          </v-flex>
-                        </v-layout>
-                      </v-container>
-
-                    </v-card-text>
-                    <!--                  End Công Ty-->
-
-                    <!--end card content-->
-                  </v-card>
-                </v-tab-item>
-              </v-tabs-items>
-            </v-tabs>
-          </template>
         </v-card-text>
 
         <v-card-actions>
@@ -308,111 +130,65 @@
     data() {
       return {
         search: '',
-        Job: [],
+        Company: [],
         loading: false,
         dialog: false,
-        items: [
-          'Thông Tin', 'Công Ty',
-        ],
         headers: [
-          {text: 'Tiêu đề công việc', value: 'title',},
-          {text: 'Công ty', value: 'companyByCompanyId.nameCompany', sortable: false},
-          {text: 'Ngày đăng', value: 'createdDate',},
-          {text: 'Trạng thái công việc', value: 'status', },
+          {text: 'Tên công ty', value: 'nameCompany',},
+          {text: 'Thành phố', value: 'cityByCityId.fullName', sortable: false},
+          {text: 'Ngày tạo', value: 'createdDate',},
+          {text: 'Trạng thái công ty', value: 'status', },
           {text: 'Thao tác', sortable: false},
         ],
         pagination: {
-          sortBy: 'title',
+          sortBy: 'nameCompany',
           ascending: true
         },
-        jobFull: {
-          id : '',
-          userId: '',
-          companyId:'',
-          company: {
-            id: '',
-            nameCompany: '',
-            cityId: '',
+        companyDetail:{
             address: '',
-            telephoneNumber: '',
-            email: '',
-            logoImg: '',
-            description: '',
+            address: '',
+            cityByCityId: {
+              fullName: '',
+              id: ''
+            },
+            cityId: '',
+            companyindustriesById: '',
             createdDate: '',
-            lastModify:'',
-            status: '',
-            companyindustriesById: []
-          },
-          title: '',
-          cityId:'',
-          industryId: '',
-          city:{
+            description: '',
+            email: '',
             id: '',
-            fullName: ''},
-          address: '',
-          joblevelId: '',
-          joblevelName: '',
-          workingtype: '',
-          numberofrecruitment: '',
-          salaryFrom: '',
-          salaryTo: '',
-          yearExperience: '',
-          createdDate: '',
-          endDateForApply: '',
-          jobDescription: '',
-          additionalRequest: '',
-          candidateBenefits: '',
-          status: '',
-          listSkillName: [],
-          listJobSameCompany: []
+            lastModify: '',
+            logoImg: '',
+            nameCompany: '',
+            status: '',
+            telephoneNumber: ''
         },
 
       }
     },
     methods: {
-      async viewInfo(id){
-        this.loading = true;
-        await Axios.get(`http://localhost:8080/job/getJobDetail?id=${id}`)
-          .then(response => {
-            this.jobFull = response.data.data;
-            this.jobFull.createdDate = this.moment(this.jobFull.createdDate).format('DD-MM-YYYY');
-            this.jobFull.endDateForApply = this.moment(this.jobFull.endDateForApply).format('DD-MM-YYYY');
-          })
-          .catch(console.error)
-          .finally(() => {
-            this.loading = false;
-          })
+       viewInfo(id){
+        for (let i = 0; i < this.Company.length; i++){
+          if(id === this.Company[i].id){
+            this.companyDetail = this.Company[i]
+          }
+        }
       },
-      getAllJob() {
-
-        // if (this.search) {
-        Axios.get('http://localhost:8080/job/getAllJob?search=' + this.search + '&status=')
+      getAllCompany() {
+        this.loading = true;
+        Axios.get('http://localhost:8080/company/getCompanyAdmin?search=' + this.search + '&status=')
           .then(response => {
-            this.Job = response.data.data.content;
-            // this.pagination.totalItems =  this.Job.length
+            this.Company = response.data.data.content;
+            console.log(this.Company)
           })
-          .catch(err => console.log(err.response.data))
+          .catch(err => console.log(err))
           .finally(() => this.loading = false);
-        // }
-        // if(this.pagination){
-        //   this.loading = true;
-        //   Axios.get(`http://localhost:8080/job/getAllJob`)
-        //     .then(response => {
-        //       // console.log(response)
-        //       this.Job = response.data.data.content;
-        //       console.log("abc")
-        //     })
-        //     .catch(console.error)
-        //     .finally(() => {
-        //       this.loading = false;
-        //     })
-        // }
       },
 
     },
     watch: {
       pagination() {
-        this.getAllJob()
+        this.getAllCompany()
       },
     },
     beforeMount(){
